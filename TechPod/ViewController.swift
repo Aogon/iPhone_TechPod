7ddb7731b025861e75a4f76bcc133687ca20e7f4
AoiSakai <aoi_sakai@icloud.com>
2021-02-06 17:35:16 +0900

    音楽再生アプリの完成

--- TechPod/ViewController.swift
+++ TechPod/ViewController.swift
@@ -6,12 +6,50 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    //StoryBoardで扱うTableViewを宣言
+    @IBOutlet var table: UITableView!
+    
+    var songNameArray = [String]()
+    var fileNameArray = [String]()
+    var imageNameArray = [String]()
+    var audioPlayer: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //テープルビューのデータソースメソッドはViewControllerクラスに書くよ、という設定
+        table.dataSource = self
+        
+        //UITableViewが持っているデリゲートメソッドの処理の委託先をself(ViewController.swift)に設定
+        table.delegate = self
+        songNameArray = ["カノン", "エリーゼのために", "G線上のアリア"]
+        fileNameArray = ["cannon", "elise", "aria"]
+        imageNameArray = ["Pachelbel.jpg", "Beethoven.jpg", "Bach.jpg"]
+    }
+    
+    //セルの数を設定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songNameArray.count
+    }
+    
+    //ID付きのセルを取得して、セル付属のtextLabelに「テスト」と表示させてみる
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = songNameArray[indexPath.row]
+        cell?.imageView?.image = UIImage(named: imageNameArray[indexPath.row])
+        return cell!
+    }
+    
+    //セルが押された時に呼ばれるデリゲートメソッド
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(songNameArray[indexPath.row])が選ばれました！")
+        
+        let audioPath = URL(fileURLWithPath: Bundle.main.path(forResource:fileNameArray[indexPath.row], ofType: "mp3")!)
+        audioPlayer = try? AVAudioPlayer(contentsOf: audioPath)
+        audioPlayer.play()
     }
 
 
